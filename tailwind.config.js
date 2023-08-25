ee8f4b249261bc394fac5c04c34a9d0231841091
LazyCoder-1506 <pranayan1506@gmail.com>
2023-08-25 10:13:19 +0530

    project installation and color setup done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#7000DA',
+        'secondary': '#12CA3E'
+      }
+    },
+  },
+  plugins: [],
+}
